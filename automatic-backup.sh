51fcbd238354208f445e7b77ca43aa68f3ec99bb
exavierace.munar <toro.exavierace.munar@gmail.com>
2018-08-03 02:20:25 -0400

    This performs an initial and full backup every 5 minutes

--- automatic-backup.sh
+++ automatic-backup.sh
@@ -3,22 +3,22 @@
 		#5 minutes interval
 
 		set(){
-							 mail="MAILTO="
-							 path="PATH=/sbin:/bin:/usr/sbin:/usr/bin"
+               mail="MAILTO="
+               path="PATH=/sbin:/bin:/usr/sbin:/usr/bin"
                croncmd="/usr/bin/bash /usr/bin/automatic-backup.sh"
                cronjob="*/5 * * * * $croncmd"
 
-	             s=$(service crond start)
-               c=$(crontab -l | grep -v -F "$croncmd" ; echo -e "$mail\n$path\n$cronjob" | crontab - )
+	       s=$(service crond start > /dev/null 2>&1)
+               c=$(crontab -l > /dev/null 2>&1 | grep -v -F "$croncmd" ; echo -e "$mail\n$path\n$cronjob" | crontab - )
 
            }
 	       set
-				 echo -e "\e[4;32mUPDATING LOCAL FILE DATABASE..\e[0m"
+	        echo -e "\e[4;32mUPDATING LOCAL FILE DATABASE..\e[0m"
 		# Database backup to dump-backup folder
 		pass="Toro_sysad1"
-		backup=$(mysqldump -u root -p$pass --databases important_files > /opt/dump-backup/important_files.sql)
+		backup=$(mysqldump -u root -p$pass --databases important_files > /opt/dump-backup/important_files.sql > /dev/null 2>&1)
 
-			$(tar cvf /opt/dump-backup.tar /opt/dump-backup) #Archive the folder dump-backup
+		$(tar cvf /opt/dump-backup.tar /opt/dump-backup > /dev/null 2>&1) #Archive the folder dump-backup
 
 			#backup
 			setbackup(){
@@ -30,9 +30,9 @@
 
 
 				#incremental backup
-				rsync -avr --numeric-ids /opt/dump-backup /opt/incremental-backup/$TIMEDATE
+				rsync -avr --numeric-ids /opt/dump-backup /opt/incremental-backup/$TIMEDATE > /dev/null 2>&1
 				#full backup
-				rsync -avr /opt/dump-backup /opt/full-backup
+				rsync -avr /opt/dump-backup /opt/full-backup > /dev/null 2>&1
 
 		}
 		echo -e "\e[0;32mUPDATED!\e[0m" "\n"
@@ -42,11 +42,31 @@
 			setbackup #calls the backup
 			#notif email
 			echo -e "\e[1;34mUPDATED!\e[0m" "\n"
-			echo -e "\e[1;33mSending email @gmail.com..\e[0m"
+			echo "Please wait.."
+			#sendemailalternative bar
+			sendemail(){
+			sendingemail="Sending email at toro.exavierace.munar@gmail.com"
+			function printProgressBar() {
+			    local progressBar="."
+			    printf "%s" "${progressBar}"
+			}
+			printf "%s" "${sendingemail}"
+			while (( cnt < 5))
+			do
+			    ((cnt++))
+			    printProgressBar
+			    sleep 1
+			done
+			# Email status
+			sendingstatus="Sent"
+			printf " [%s]\n" "${sendingstatus}"
+			}
 			sm=$(echo -e 'Subject: DB_Backup\n\nYour database have been backup successfully.' | ssmtp toro.exavierace.munar@gmail.com)
-			echo -e "\e[1;33mSent!\e[0m" "\n"
-		echo -e "The default schedule for the script to run is set to 5 minutes" "\n"
+			sendemail
 
+		# echo -e "\e[1;33mSent!\e[0m" "\n"
+		echo -e "The default schedule for the script to run is set to 5 minutes" "\n"
+		#setting schedule
 		setsched(){
 			echo -e "\e[1;37mTASK SCHEDULER\e[0m"
 			echo -e "\e[1;37m--------------\e[0m"
@@ -87,7 +107,8 @@
 			c=$(crontab -r | grep -v -F "$croncmd" ; echo -e "$mail\n$path\n$cronjob" | crontab - )
 			echo -e "\e[1;37mUPDATED!\e[0m"
 		}
-		while true; do
+    #udpate shedule
+    while true; do
     read -p "Do you wish to update the schedule? " yn
     case $yn in
         [Yy]* ) setsched; break;;
